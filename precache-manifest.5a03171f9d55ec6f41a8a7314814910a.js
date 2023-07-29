7aa8275998dc76c1e4ce80264a2e893f7e733af5
Rudra <102217181+Rudra1202@users.noreply.github.com>
2023-07-29 17:34:58 +0530

    Updates

---
+++ precache-manifest.5a03171f9d55ec6f41a8a7314814910a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb71f5170265868a51d8fcf10e31213",
+    "url": "/React-Keeper-Notes/index.html"
+  },
+  {
+    "revision": "b1b29cf9efb7207dbe7d",
+    "url": "/React-Keeper-Notes/static/js/2.df0d89d3.chunk.js"
+  },
+  {
+    "revision": "cc27ab4e5b187e0b92e6",
+    "url": "/React-Keeper-Notes/static/js/main.9a381fa0.chunk.js"
+  },
+  {
+    "revision": "7ddc7428ebd2b31ea7c0",
+    "url": "/React-Keeper-Notes/static/js/runtime-main.ea8af2b9.js"
+  }
+]);
